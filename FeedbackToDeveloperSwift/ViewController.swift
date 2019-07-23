0a9d431635756fdf65ca6074a99263e71cf25f47
Vlad Kalaev <kalaevwlad@gmail.com>
2019-07-23 16:21:36 +0300

    Initial Commit

---
+++ FeedbackToDeveloperSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FeedbackToDeveloperSwift
+//
+//  Created by Влад Калаев on 23/07/2019.
+//  Copyright © 2019 Vlad Kalaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
